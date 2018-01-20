1326f51cbdda6ac29578658ed97393394c4c2f7c
chengdol <chengdol@usc.edu>
2018-01-20 16:08:34 +0800

    soctket program

---
+++ src/Socket_program/Client.java
@@ -0,0 +1,77 @@
+package Socket_program;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+
+// client
+// send message from terminal to socket
+// type "over" to terminate connection
+public class Client
+{
+	// socket
+	private Socket socket = null;
+	// input stream
+	// example use DateInputStream
+	// but the readLine() was deprecated
+	private BufferedReader in = null;
+	// output stream
+	private DataOutputStream out = null;
+	
+	
+	public Client(String address, int port)
+	{
+		try
+		{
+			// establish a connection
+			socket = new Socket(address, port);
+			System.out.println("Client: Connected");
+			
+			// set input from terminal
+			in = new BufferedReader(new InputStreamReader(System.in));
+			// send output to socket
+			out = new DataOutputStream(socket.getOutputStream());
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+		// read from terminal
+		String line = "";
+		// end when type "over"
+		while (!line.equals("over"))
+		{
+			try
+			{
+				
+				line = in.readLine();
+				out.writeUTF(line);
+			} catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+			
+		}
+		
+		try
+		{
+			// close
+			socket.close();
+			in.close();
+			out.close();
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+	}
+
+	public static void main(String[] args)
+	{
+		// start connection
+		Client client = new Client("127.0.0.1", 5000);
+	}
+}
---
+++ src/Socket_program/Server.java
@@ -0,0 +1,56 @@
+package Socket_program;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+// server
+public class Server
+{
+	private Socket socket;
+	private ServerSocket server;
+	private DataInputStream in;
+	
+	public Server(int port)
+	{
+		try
+		{
+			// start server and wait for connection...
+			server = new ServerSocket(port);
+			System.out.println("Server: start service");
+			System.out.println("Server: wait for connection...");
+			
+			// block until connection established
+			socket = server.accept();
+			System.out.println("Server: client accepted!");
+			
+			// decorator pattern
+			// take input from socket
+			in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+			
+			String line = "";
+			while (!line.equals("over"))
+			{
+				line = in.readUTF();
+				System.out.println(">>> " + line);
+			}
+			
+			// close connection
+			System.out.println("Server: connection closed!");
+			socket.close();
+			in.close();
+			
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		Server server = new Server(5000);
+	}
+	
+}
