ca176d483cd3560072d74270ecbfc15302e328f1
chengdol <chengdol@usc.edu>
2018-03-01 11:04:04 -0800

    logger

--- src/Threads_with_Socket/ThreadPoolServerSocket.java
+++ src/Threads_with_Socket/ThreadPoolServerSocket.java
@@ -10,15 +10,64 @@ import java.util.Date;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 // 这种结构比较好
 // don't use try-with for child socket
 // use executor limit thread pool size
 
 // this is a time server example
+// add logging to it
 public class ThreadPoolServerSocket
 {
-	private final static int PORT = 1991;
+	private final static int PORT = 1992;
+	// logging
+	// 这里没有配置，输出在console
+	private final static Logger auditLogger = Logger.getLogger("request");
+	private final static Logger errorLogger = Logger.getLogger("error");
+	
+	
+	private static class ChildSocket implements Runnable
+	{
+		private Socket socket;
+		
+		public ChildSocket(Socket socket)
+		{
+			super();
+			this.socket = socket;
+		}
+
+		@Override
+		public void run()
+		{
+			try(Writer writer = new BufferedWriter(
+									new OutputStreamWriter(
+											socket.getOutputStream())))
+			{
+				Date current = new Date();
+				auditLogger.info(current + " " + socket.getRemoteSocketAddress());
+				
+				// mimic processing time
+				TimeUnit.SECONDS.sleep(2);
+				writer.write("\r\n");
+				writer.write("Time: " + current + "\r\n");
+				
+				// send data
+				writer.flush();
+				writer.close();
+			} 
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			} catch (InterruptedException e)
+			{
+				e.printStackTrace();
+			}	
+			
+		}
+	}
+
 	
 	public static void main(String[] args)
 	{
@@ -33,7 +82,6 @@ public class ThreadPoolServerSocket
 				// using regular try catch block
 				try
 				{
-					System.out.println("Server: waiting request...");
 					Socket child = server.accept();
 					// create new task
 					ChildSocket thread = new ChildSocket(child);
@@ -44,14 +92,22 @@ public class ThreadPoolServerSocket
 				// this catch for child socket when accept
 				catch(IOException e)
 				{
-					System.out.println("Server: child socket crash...");
+					errorLogger.log(Level.SEVERE, "accept error", e);
+				}
+				catch(RuntimeException e)
+				{
+					errorLogger.log(Level.SEVERE, "unexpected error " + e.getMessage(), e);					
 				}
 			}
 			
-		} catch (IOException e)
+		} 
+		catch (IOException e)
 		{
-			System.out.println("Server: cannot start server...");
-			e.printStackTrace();
+			errorLogger.log(Level.SEVERE, "could not start server", e);					
+		}
+		catch(RuntimeException e)
+		{
+			errorLogger.log(Level.SEVERE, "could not start server " + e.getMessage(), e);								
 		}
 		
 		pool.shutdown();
@@ -59,43 +115,6 @@ public class ThreadPoolServerSocket
 }
 
 
-class ChildSocket implements Runnable
-{
-	private Socket socket;
-	
-	public ChildSocket(Socket socket)
-	{
-		super();
-		this.socket = socket;
-	}
-
-	@Override
-	public void run()
-	{
-		try(Writer writer = new BufferedWriter(
-								new OutputStreamWriter(
-										socket.getOutputStream())))
-		{
-			Date current = new Date();
-			// mimic processing time
-			TimeUnit.SECONDS.sleep(3);
-			writer.write("\r\n");
-			writer.write("Time: " + current + "\r\n");
-			
-			// send data
-			writer.flush();
-			writer.close();
-		} 
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		} catch (InterruptedException e)
-		{
-			e.printStackTrace();
-		}	
-		
-	}
-}
 
 
 
