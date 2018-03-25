79d8cdf7684ba0c843fb99e07248c000fd478b16
chengdol <chengdol@usc.edu>
2018-03-25 13:59:56 -0700

    udp abstract server

---
+++ src/udp/UDPServer.java
@@ -0,0 +1,74 @@
+package udp;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+// this is a abstract server class
+public abstract class UDPServer implements Runnable
+{
+	private final int bufferSize;
+	private final int port;
+	private final Logger logger = Logger.getLogger(UDPServer.class.getCanonicalName());
+	// volatile
+	private volatile boolean isShutDown = false;
+	
+	public UDPServer(int bufferSize, int port)
+	{
+		this.bufferSize = bufferSize;
+		this.port = port;
+	}
+	
+	public UDPServer(int port)
+	{
+		this(8192, port);
+	}
+	
+
+	@Override
+	public void run()
+	{
+		byte[] buffer = new byte[bufferSize];
+		try (DatagramSocket ds = new DatagramSocket(port))
+		{
+			ds.setSoTimeout(10000);
+			DatagramPacket dpr = new DatagramPacket(buffer, bufferSize);
+			while (true)
+			{
+				if (isShutDown) { return; }
+				// wait for receiving
+				try
+				{
+					// may be timeout
+					ds.receive(dpr);
+					respond(ds, dpr);
+				} 
+				// check shutdown if timeout
+				catch (SocketTimeoutException e)
+				{
+					if (isShutDown) return;
+				}
+				catch (IOException e)
+				{
+					logger.log(Level.WARNING, e.getMessage(), e);
+				}
+				
+			}	
+		} catch (SocketException e)
+		{
+			logger.log(Level.SEVERE, e.getMessage(), e);
+		} 
+	}
+	
+	// abstract method
+	public abstract void respond(DatagramSocket ds, DatagramPacket dp);
+	
+	public void setShutDown()
+	{
+		isShutDown = true;
+	}
+}
